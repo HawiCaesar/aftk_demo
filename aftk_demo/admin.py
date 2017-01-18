efd5ed918432bc8c378d7ab960ffdcab302f5701
HawiCaesar <brian.odhiambo932@gmail.com>
2017-01-18 19:19:47 +0300

    index file, Africas Talking API, data in db

--- aftk_demo/admin.py
+++ aftk_demo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from models import Dispense_Drug_List
 
 # Register your models here.
+
+admin.site.register(Dispense_Drug_List)
--- aftk_demo/views.py
+++ aftk_demo/views.py
@@ -1,9 +1,53 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from models import Dispense_Drug_List
+from django.template import loader
+from AfricasTalkingGateway import AfricasTalkingGateway, AfricasTalkingGatewayException
+from django.http import JsonResponse
 
 # Create your views here.
 
+def index(request):
+
+	list_of_persons = Dispense_Drug_List.objects.all()
+
+	return render(request, 'index.html', {'sms_list': list_of_persons})
+
 def demo_dashboard(request):
 
-	render(request, 'demo_dashboard.html')
+	return render(request, 'demo_dashboard.html')
+
+
+def send_sms(request):
+
+	username = "XXXXXXXXXXXXXXXXXX"
+	apikey   = "XXXXXXXXXXXXXXXXXX"
+
+	to = "+254XXXXXXXX"
+
+	message = "I'm a lumberjack and it's ok, I sleep all night and I work all day"
+
+	gateway = AfricasTalkingGateway(username, apikey)
+
+	try:
+		results = gateway.sendMessage(to2, message)
+
+		for recipient in results:
+
+		 	print 'number=%s;status=%s;messageId=%s;cost=%s' % (recipient['number'],
+	                                                            recipient['status'],
+	                                                            recipient['messageId'],
+	                                                            recipient['cost'])
+
+		response = JsonResponse({'sms_status': 'sms sent to ' + str(to2)})
+
+		return HttpResponse(response, content_type='application/json')
+
+	except AfricasTalkingGatewayException, e:
+
+		error = 'Encountered an error while sending: %s' % str(e)
+
+	 	return HttpResponse(error, content_type='application/json')
+
+	
 
