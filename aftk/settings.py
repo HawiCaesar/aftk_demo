0e9eceeccc472c6a3c6b78e8caf275135896e74e
HawiCaesar <brian.odhiambo932@gmail.com>
2017-03-15 21:58:45 +0300

    settings.py changes

--- aftk/settings.py
+++ aftk/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'NAME': 'africastalking',
-        'USER': '..',
+        'USER': '.',
         'PASSWORD': '',
         'HOST': 'localhost'
     }
